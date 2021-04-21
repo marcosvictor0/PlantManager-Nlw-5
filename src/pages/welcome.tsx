443b67f469b44796212f288fc14a71e760ce087d
marcosvictor0 <marcos_victor159@hotmail.com>
2021-04-21 14:25:05 -0300

    att do css na parte do welcome

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 import wateringImg from '/GitHub/PlantManager/my-project/assets/watering.png';
 
 export function Welcome() {
     return(
-        <View>
+        <View style={styles.container}>
             <Text>
                 Gerencie suas plantas de forma fácil
             </Text>
             <Image source={ wateringImg } />
+            <Text>
+                Não esqueça mais de regar suas plantas. 
+                Nós cuidamos de lembrar você sempre que precisar.
+            </Text>
+
+            <TouchableOpacity>
+                <Text>
+                    >
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }
+})
