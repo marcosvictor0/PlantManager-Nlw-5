9c3b081bb71919e8fe76e91ad4ed36b158f77701
marcosvictor0 <marcos_victor159@hotmail.com>
2021-04-21 20:52:35 -0300

    add algumas API, e adicionado a pasta Fonts

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
+import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import colors from '/GitHub/PlantManager/my-project/src/pages/styles/colors';
 import wateringImg from '/GitHub/PlantManager/my-project/assets/watering.png';
 
 export function Welcome() {
     return(
-        <View style={styles.container}>
-            <Text>
-                Gerencie suas plantas de forma fácil
+        <SafeAreaView style={styles.container}>
+
+            <Text style={ styles.title }>
+                Gerencie { '\n' }
+                suas plantas { '\n' }
+                de forma fácil{ '\n' }
             </Text>
-            <Image source={ wateringImg } />
-            <Text>
+
+            <Image 
+            style={ styles.image } 
+            source={ wateringImg } 
+            resizeMode='contain'
+            />
+
+            <Text style={ styles.subtitle }>
                 Não esqueça mais de regar suas plantas. 
                 Nós cuidamos de lembrar você sempre que precisar.
             </Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity 
+            style={styles.button} 
+            activeOpacity={0.7}>
                 <Text>
-                    >
+                    <Entypo 
+                    name="chevron-thin-right"
+                    style={styles.buttonIcon}
+                    />
                 </Text>
             </TouchableOpacity>
-        </View>
+
+        </SafeAreaView>
     )
 }
 
@@ -28,6 +44,40 @@ const styles = StyleSheet.create ({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
+    },
+
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38,
+    },
+
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading,
+    },
+
+    image: {
+        height: Dimensions.get('window').width * 0.7
+    },
+
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56,
+    },
+
+    buttonIcon: {
+        color: colors.white,
+        fontSize: 26,
     }
 })
