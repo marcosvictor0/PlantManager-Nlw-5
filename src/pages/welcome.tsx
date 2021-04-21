ad20484f93a248cfed2f68c3a676d5dce9b7cf3c
marcosvictor0 <marcos_victor159@hotmail.com>
2021-04-21 14:14:44 -0300

    att

---
+++ src/pages/welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import wateringImg from '/GitHub/PlantManager/my-project/assets/watering.png';
+
+export function Welcome() {
+    return(
+        <View>
+            <Text>
+                Gerencie suas plantas de forma fácil
+            </Text>
+            <Image source={ wateringImg } />
+        </View>
+    )
+}
